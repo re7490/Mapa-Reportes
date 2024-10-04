0343f477459be56f955f8f8349586fd41e503401
re7490 <ralonsopadilla995@gmail.com>
2024-10-04 15:38:43 -0300

    registro basico de usuario

--- mapareportes/urls.py
+++ mapareportes/urls.py
@@ -20,5 +20,6 @@ from reporte import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hola)
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup')
 ]
--- reporte/views.py
+++ reporte/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
-def hola(request):
-    return render(request, 'signup.html',{
-        'form' :UserCreationForm
-    })
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    
+    if request.method =='GET':
+        return render(request, 'signup.html',{
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                usuario = User.objects.create_user(username=request.POST['username'], password=request.POST['password2'])
+                usuario.save()
+                return HttpResponse('Usuario creado :)')
+            except:
+                return HttpResponse('El usuario ya existe')
+        return HttpResponse('contraseña no coincide')
