de348ff1b47456edc2d794d1c42d37faa3fd9a32
re7490 <ralonsopadilla995@gmail.com>
2024-10-10 16:29:01 -0300

    Botones para seleccionar piso al inicio y cuando se ve el mapa

--- mapareportes/urls.py
+++ mapareportes/urls.py
@@ -26,5 +26,10 @@ urlpatterns = [
     path('crear_reporte/', views.crear_reporte, name='crear_reporte'),
     path('reportes/', views.reportes, name='reportes'),
     path('reportes/<int:id>/', views.reporte_, name='reporte_'),
-    path('reportes/<int:id>/borrar_reporte', views.borrar_reporte, name='borrar_reporte')
+    path('reportes/<int:id>/borrar_reporte', views.borrar_reporte, name='borrar_reporte'),
+    path('piso-1/', views.mapa_piso0, name='piso-1'),
+    path('piso1/', views.mapa_piso1, name='piso1'),
+    path('piso2/', views.mapa_piso2, name='piso2'),
+    path('piso4/', views.mapa_piso4, name='piso4'),
+        
 ]
