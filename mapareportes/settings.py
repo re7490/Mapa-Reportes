065f7ddbc253512dd9d6de026a5fdc3bdaf987e4
re7490 <ralonsopadilla995@gmail.com>
2024-11-02 12:47:48 -0300

    boton cerrar sesion

--- mapareportes/settings.py
+++ mapareportes/settings.py
@@ -130,5 +130,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGOUT_REDIRECT_URL= 'login'
+LOGOUT_REDIRECT_URL= 'home'
 LOGIN_REDIRECT_URL= 'postlogin'
+LOGIN_URL = 'login'  
