02a14a421f216c46c682a27640c3d40c00eb5b3b
re7490 <ralonsopadilla995@gmail.com>
2024-11-19 20:44:39 -0300

    cambio de color y añadi cosas a la vista de los usuarios

--- reporte/urls.py
+++ reporte/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('piso-1/', views.mapa_piso0, name='piso-1'),
     path('piso1/', views.mapa_piso1, name='piso1'),
     path('piso2/', views.mapa_piso2, name='piso2'),
-    path('registrar/', registrar, name='registro'),
+    path('registro/', registrar, name='registro'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', success_url='/postlogin/'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('postlogin/', views.postlogin,name='postlogin'),
